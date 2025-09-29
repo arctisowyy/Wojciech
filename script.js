6d0a3e81402e44bee8686c15d6c89251e166548c
arctisowyy <arctisowyy@gmail.com>
2025-09-29 21:51:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const secondImage = "wojciech.png";
 const rejectImage = "image.png";
 
 function handleAccept() {
-  document.getElementById('preview').src = secondImage;
+  document.getElementById('preview').src = "wojciech.png";
   document.getElementById('buttons').style.display = 'none';
   document.getElementById('result').innerHTML =
     `Dziękuję kociaku ❤️ <br><span class="happy">Wojciech jest very happy 😊</span>`;
 }
 
 function handleReject() {
-  document.getElementById('preview').src = rejectImage;
+  document.getElementById('preview').src = "image.png";
   document.getElementById('buttons').style.display = 'none';
   document.getElementById('result').innerHTML =
     `Propozycja odrzucona 💔 <br><span class="sad">Wojciech jest very sad 😢</span>`;
 }
+
 
